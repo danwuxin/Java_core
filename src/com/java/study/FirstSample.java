e6b1c9e4efec6d7a1556abb0e8677aa3b91433a8
danwuxin <danwuxin@126.com>
2020-02-19 23:18:11 +0800

    init

---
+++ src/com/java/study/FirstSample.java
@@ -0,0 +1,14 @@
+package com.java.study;
+
+/**
+ * @author 淡无心
+ * @create 2020/2/19
+ * @since 1.0.0
+ */
+public class FirstSample {
+
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello world!'");
+    }
+
+}
